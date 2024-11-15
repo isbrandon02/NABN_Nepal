74c8613800db5d8975509a26ba6c87f887dc65d4
Amogh Manral <amogh.manral@duke.edu>
2024-11-14 22:19:12 -0500

    added photo

--- CoSoG_Nepal/src/App.jsx
+++ CoSoG_Nepal/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from "./pages/Home.jsx"
+import AboutUs from './pages/AboutUs.jsx'
 
 function App() {
   return (
     <>
       <Home />
+      <AboutUs />
     </>
   )
 }
