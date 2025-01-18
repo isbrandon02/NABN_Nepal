3bb1acaa8e0fca606b5d48df6405e7c328ff0410
isbrandon02 <brandon.chng1102@gmail.com>
2025-01-18 18:19:08 -0500

    fixed eventspage

--- CoSoG_Nepal/src/pages/AboutUs.jsx
+++ CoSoG_Nepal/src/pages/AboutUs.jsx
@@ -21,17 +21,7 @@ function AboutUs() {
           <h1>Who we are?</h1>
           
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-minima nesciunt dolorem!         </p>
+          The National Adolescent Boys Network Nepal (NABN) was founded on 14th Shrawan, 2078 (2021) with the primary objective of ensuring the comprehensive rights of children and adolescents, advocating for gender equality, ending gender-based violence, and empowering the active participation of adolescents in these efforts. The network was established with the clear vision that youth involvement is essential for achieving meaningful change in society.         </p>
           
           <p>
           Officiis iure rerum voluptates a cumque velit 
